cca5d5a6776956fd58af2319360c5111c5ae3108
zlurker <tallspin_97@hotmail.com>
2021-09-16 23:34:53 +0800

    completed the loop without having two elements left over
    Need to re-read the proper code to see what is missing in mine...

--- realm-of-madness/realm-of-madness/SpaceMatrix.cpp
+++ realm-of-madness/realm-of-madness/SpaceMatrix.cpp
@@ -28,19 +28,18 @@ void SpaceMatrix::SetMatrixElementLocation(int elementId, Vector2 newCoords) {
 		int* axisPos = matrixEle->GetAxisPosition(i);
 		bool newElement = *axisPos == -1;
 
-		if (!newElement) {
-			std::cout << "existing element" << std::endl;
+		if (!newElement)
 			if (currCoords[i] > newCoords[i])
 				rangeEnd = *axisPos;
 			else
 				rangeStart = *axisPos;
-		}
-		else
-			std::cout << "new element" << std::endl;
 
 		std::cout << "Range start: " << rangeStart << " Range End: " << rangeEnd << std::endl;
 		int insertPos = BinarySearch(rangeStart, rangeEnd, newCoords[i], i);
-		//std::cout << "Exited search" << std::endl;
+
+		// Can only standardise this part for insertpos. For pos > size, move and insert handles differently and as such, we will handle it in its function.
+		if (0 > insertPos)
+			insertPos = 0;
 
 		if (!newElement)
 			insertPos = MoveAxisElement(i, *axisPos, insertPos);
@@ -48,7 +47,6 @@ void SpaceMatrix::SetMatrixElementLocation(int elementId, Vector2 newCoords) {
 			insertPos = InsertAxisElement(i, insertPos, elementId);
 
 		std::cout << "Axis: " << i << " Insert Pos: " << insertPos << " Value: " << newCoords[i] << std::endl;
-
 		*axisPos = insertPos;
 	}
 
@@ -68,30 +66,47 @@ int* SpaceMatrix::GetElementsInRange(Vector2 startRange, Vector2 endRange) {
 }
 
 int SpaceMatrix::BinarySearch(int rangeStart, int rangeEnd, float value, int axis) {
-	int difference = (rangeEnd - rangeStart) / 2;
-	//std::cout << "RS: " << rangeStart << " RE: " << rangeEnd << std::endl;
+	//int difference = (rangeEnd - rangeStart) / 2;
+	std::cout << "RS: " << rangeStart << " RE: " << rangeEnd << std::endl;
 
-	if (rangeEnd - rangeStart <= 1)
+	if (rangeEnd == rangeStart)
 		return rangeStart + DetermineBinaryRange(rangeStart, value, axis);
 
-	int axisCenterPoint = rangeStart + difference;
+	int axisCenterPoint = (rangeStart + rangeEnd) / 2;
+	int processedCenterPoint = axisCenterPoint;
 	int binaryRangeResult = DetermineBinaryRange(ReturnAxisElement(axis, axisCenterPoint), value, axis);
 
-	if (binaryRangeResult == -1)
-		return BinarySearch(rangeStart, axisCenterPoint, value, axis);
+	if (binaryRangeResult == -1) {
+		processedCenterPoint--;
 
-	return BinarySearch(axisCenterPoint, rangeEnd, value, axis);
+		if (processedCenterPoint < rangeStart)
+			processedCenterPoint = rangeStart;
+
+		return BinarySearch(rangeStart, processedCenterPoint, value, axis);
+	}
+
+	if (binaryRangeResult == 1) {
+		processedCenterPoint++;
+
+		if (processedCenterPoint > rangeEnd)
+			processedCenterPoint = rangeEnd;
+		return BinarySearch(processedCenterPoint, rangeEnd, value, axis);
+	}
+
+	return axisCenterPoint;
 }
 
 int SpaceMatrix::DetermineBinaryRange(int centerPoint, float value, int axis) {
 	if (matrixElements[centerPoint].coordinates[axis] > value)
 		return -1;
+	if (matrixElements[centerPoint].coordinates[axis] < value)
+		return 1;
 
-	return 1;
+	return 0;
 }
 
 int SpaceMatrix::ReturnAxisElement(int axis, int elementId) {
-	//std::cout << elementId << (*xAxis).size() << std::endl;
+	std::cout << elementId << (*xAxis).size() << std::endl;
 
 	if (axis == 0)
 		return (*xAxis)[elementId];
@@ -102,15 +117,11 @@ int SpaceMatrix::ReturnAxisElement(int axis, int elementId) {
 int SpaceMatrix::MoveAxisElement(int axis, int current, int next) {
 	std::vector<int>* selectedAxis = ReturnAxis(axis);
 
-	if (0 > next)
-		next = 0;
-
 	if (next >= selectedAxis->size())
 		next = selectedAxis->size() - 1;
 
 	VectorHelpers::MoveVectorElement(selectedAxis, current, next);
 	return next;
-	//std::rotate()
 }
 
 int SpaceMatrix::InsertAxisElement(int axis, int pos, int value) {
@@ -118,15 +129,8 @@ int SpaceMatrix::InsertAxisElement(int axis, int pos, int value) {
 	std::vector<int>* selectedAxis = ReturnAxis(axis);
 	std::cout << "insert pos: " << pos << " axis size: " << selectedAxis->size() << std::endl;
 
-	// Compares if it is negative first as we are comparing against unsigned.
-	if (0 > pos) {
-		std::cout << "Appending element to front" << std::endl;
-		selectedAxis->insert(selectedAxis->begin(), value);
-		return 0;
-	}
-
 	if (pos >= selectedAxis->size()) {
-		std::cout << "Pushing back element" << std::endl;
+		//std::cout << "Pushing back element" << std::endl;
 		selectedAxis->push_back(value);
 		return selectedAxis->size() - 1;
 	}
